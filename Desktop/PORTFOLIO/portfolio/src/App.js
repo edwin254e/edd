9e70b183286108e24a7c9b0980f725fa1e6a542a
edwin254e <edwinmuliro64@gmail.com>
2019-12-19 10:37:19 +0300

    my first commit

---
+++ Desktop/PORTFOLIO/portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home';
+
+function App() {
+ 
+
+  
+  return (
+    <div className="App">
+      <Home/>
+  <h1>keny</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/PORTFOLIO/portfolio/src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './main.css';
+
+function Home(){
+    return(
+<div>
+   <div className="main">
+       <div className="mainContent">
+           <div className="homePic">
+<h1>pic here</h1>
+           </div>
+           <div className="homeMessage">
+<p>messge here</p>
+           </div>
+       </div>
+   </div>
+</div>
+    );
+}
+export default Home;
